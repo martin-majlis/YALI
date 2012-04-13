5822ab038162341f39613959e8dfadacc2c0e5b1
Martin Majlis <majlis@ufal.mff.cuni.cz>
2012-04-13 15:38:46 +0200

    Added file with common variables.

---
+++ tests/test-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE_TRAIN_A=test_train_aaa
+FILE_TRAIN_B=test_train_bbb
+FILE_TRAIN_FL=test_fl
+FILE_LANGS=test_langs
+DIR_MODEL=test_model
+
+DIR_TEST=test_test
+FILE_TEST_A=$DIR_TEST/test_aaa
+FILE_TEST_B=$DIR_TEST/test_bbb
